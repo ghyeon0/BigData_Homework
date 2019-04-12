be82f19fc78d96afe0a7f9599fa6258ceffc3f40
ghyeon0 <xcm1321@gmail.com>
2019-04-12 20:02:52 +0900

    test multiprocessing

--- HW4/HW4_renew/generate_sentence.py
+++ HW4/HW4_renew/generate_sentence.py
@@ -1,7 +1,14 @@
 import generate_sentence_bigram
 import generate_sentence_trigram
+from multiprocessing import Process
 
 
 def generate_sentence(data):
-    generate_sentence_bigram.generate_sentence_bigram(data)
-    generate_sentence_trigram.generate_sentence_trigram(data)
+    p1 = Process(target=generate_sentence_bigram.generate_sentence_bigram, args=(data))
+    p2 = Process(target=generate_sentence_trigram.generate_sentence_trigram, args=data))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+    # generate_sentence_bigram.generate_sentence_bigram(data)
+    # generate_sentence_trigram.generate_sentence_trigram(data)
