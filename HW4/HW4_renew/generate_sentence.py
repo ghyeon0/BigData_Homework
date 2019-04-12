3dc1360ee0e0ebfc8e3aaa374c1de04199a8eaab
ghyeon0 <xcm1321@gmail.com>
2019-04-12 16:31:30 +0900

    Trigram calc probability

--- HW4/HW4_renew/generate_sentence.py
+++ HW4/HW4_renew/generate_sentence.py
@@ -49,13 +49,33 @@ def count_and_calc_probability_bigram(parsed_data):
     for master_key in count_dic.keys():
         for slave_key in probability_dic[master_key].keys():
             probability_dic[master_key][slave_key] = probability_dic[master_key][slave_key] / count_dic[master_key]
-            print(master_key, slave_key, probability_dic[master_key][slave_key])
 
     return count_dic, probability_dic
 
 
 def count_and_calc_probability_trigram(parsed_data):
-    pass
+    count_dic = {}
+    probability_dic = {}
+
+    for i, data in enumerate(parsed_data[:-2]):
+        temp_data = data + parsed_data[i + 1]
+        next_data = parsed_data[i + 2]
+        if temp_data not in count_dic:
+            count_dic[temp_data] = 1
+            probability_dic[temp_data] = {next_data: 1}
+        else:
+            count_dic[temp_data] += 1
+            if next_data not in probability_dic[temp_data]:
+                probability_dic[temp_data][next_data] = 1
+            else:
+                probability_dic[temp_data][next_data] += 1
+    
+    for master_key in count_dic.keys():
+        for slave_key in probability_dic[master_key].keys():
+            probability_dic[master_key][slave_key] = probability_dic[master_key][slave_key] / count_dic[master_key]
+            print(master_key, slave_key, probability_dic[master_key][slave_key])
+
+    return count_dic, probability_dic
 
 
 def generate_sentence_bigram(data):
